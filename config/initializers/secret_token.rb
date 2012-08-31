3b6e2f68b3de56535c3b592102f30b3af2d18fc8
Cedric Fung <wolfplanet@gmail.com>
2012-08-31 13:24:20 +0800

    $ rails new vec.io -OT --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VecIo::Application.config.secret_token = '5dbcf3d0e9cc62dbb94667b55ed03b264dd00127d00b321c88b1ad2623658cdd665024c3d9af413d02413157d0b91e8b2458d0ca7f507349f75280735494a1d7'
