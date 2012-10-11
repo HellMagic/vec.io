819a7fc399191c167167f8cbe43018b35219a8d3
Cedric Fung <root@vec.io>
2012-10-11 12:29:40 +0800

    Fix history diff bug

--- app/helpers/histories_helper.rb
+++ app/helpers/histories_helper.rb
@@ -1,12 +1,11 @@
 module HistoriesHelper
   def diff(h)
-    l1 = (h.original['title'] || h.trackable.title) + " #{h.original['updated_at']}"
-    l2 = (h.modified['title'] || h.trackable.title) + " #{h.modified['updated_at']}"
+    l1 = "#{h.original['title'] || h.trackable.title} #{h.original['updated_at']}"
+    l2 = "#{h.modified['title'] || h.trackable.title} #{h.modified['updated_at']}"
     o = h.original['content']
     m = h.modified['content']
-    m2 = m || " "
-    html = Diffy::Diff.new(o, m2, diff: ['-u', '-d', "-L#{l1}", "-L#{l2}"], include_diff_info: true, include_plus_and_minus_in_html: false, allow_empty_diff: true).to_s(:html)
-    html = html.scan(/.*UTC<\/ins><\/li>/m)[0] + "</ul></div>" unless m
+    html = Diffy::Diff.new(o, m || " ", diff: ['-u', '-d', "-L#{l1}", "-L#{l2}"], include_diff_info: true, include_plus_and_minus_in_html: false, allow_empty_diff: true).to_s(:html)
+    html = html.scan(/(.*<\/ins><\/li>)\s*<li/m)[0][0] + "</ul></div>" unless m
     html.html_safe
   end
 end
