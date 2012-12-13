fe9c30955b120d653efa446c228d65b18bb3a546
Cedric Fung <root@vec.io>
2012-12-13 21:46:59 +0800

    Shellescape the options to diff

--- app/helpers/histories_helper.rb
+++ app/helpers/histories_helper.rb
@@ -1,7 +1,7 @@
 module HistoriesHelper
   def diff(h)
-    l1 = "#{h.original['title'] || h.trackable.title} #{h.original['updated_at']}"
-    l2 = "#{h.modified['title'] || h.trackable.title} #{h.modified['updated_at']}"
+    l1 = "#{h.original['title'] || h.trackable.title} #{h.original['updated_at']}".shellescape
+    l2 = "#{h.modified['title'] || h.trackable.title} #{h.modified['updated_at']}".shellescape
     o = h.original['content']
     m = h.modified['content']
     html = Diffy::Diff.new(o, m || " ", diff: ['-u', '-d', "-L#{l1}", "-L#{l2}"], include_diff_info: true, include_plus_and_minus_in_html: false, allow_empty_diff: true).to_s(:html)
